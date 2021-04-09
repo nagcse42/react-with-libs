b1743815f0f18b676ad6de89a62e2b19684319b3
nagcse42 <nagcse42@gmail.com>
2021-04-09 22:51:17 +0400

    react modal added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import TechSkills from "./components/tech-skills";
 import NotificationToast from "./components/notifaction-toast";
+import ModelComponent from "./components/model-component";
 
 function App() {
   return (
     <div>
       <TechSkills></TechSkills>
       <NotificationToast></NotificationToast>
+      <ModelComponent></ModelComponent>
     </div>
   );
 }
---
+++ src/components/model-component.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Model from "react-modal";
+
+Model.setAppElement("#root");
+
+function ModelComponent() {
+  const [openModel, setOpenModel] = useState(false);
+  return (
+    <div>
+      <div style={{ textAlign: "center" }}>
+        <button onClick={() => setOpenModel(true)}>Open Model</button>
+      </div>
+      <Model
+        isOpen={openModel}
+        onRequestClose={() => setOpenModel(true)}
+        shouldCloseOnOverlayClick={false}
+      >
+        <h2>Model Header</h2>
+        <p>Model content</p>
+
+        <div>
+          <button onClick={() => setOpenModel(false)}>Close</button>
+        </div>
+      </Model>
+    </div>
+  );
+}
+
+export default ModelComponent;
