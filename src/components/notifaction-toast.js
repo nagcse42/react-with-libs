ad48974bf384615ca028eb973566a3eead923d76
nagcse42 <nagcse42@gmail.com>
2021-04-09 22:18:36 +0400

    Custom toast component chnages committed.

--- src/components/notifaction-toast.js
+++ src/components/notifaction-toast.js
@@ -3,20 +3,32 @@ import "react-toastify/dist/ReactToastify.css";
 
 toast.configure();
 
+const CustomNotification = ({ closeToast }) => {
+  return (
+    <div>
+      <span>Something went wrong..Contact IT</span>
+      <button onClick={closeToast}>Close</button>
+    </div>
+  );
+};
+
 function NotificationToast() {
   const notify = () => {
     toast("Hey dude...", { position: toast.POSITION.TOP_LEFT });
     toast.info("Hey dude Its info...", { position: toast.POSITION.TOP_RIGHT });
-    toast.warning("Hey dude its warning...", {
+    toast.warning(<CustomNotification />, {
       position: toast.POSITION.TOP_CENTER
     });
     toast.error("Hey dude its error...", {
       position: toast.POSITION.BOTTOM_LEFT
     });
-    toast("Hey dude...", { position: toast.POSITION.BOTTOM_CENTER });
+    toast.error(<CustomNotification />, {
+      position: toast.POSITION.BOTTOM_CENTER
+    });
+    toast.success("Hey dude...", { position: toast.POSITION.BOTTOM_RIGHT });
   };
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <button onClick={notify}>Toast </button>
     </div>
   );
