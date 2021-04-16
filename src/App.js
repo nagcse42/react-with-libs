5a7b58700dc695cdd0766051259c156f23bdddb9
nagcse42 <nagcse42@gmail.com>
2021-04-16 20:56:24 +0400

    react router added.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/home.component";
 import TechSkills from "./components/tech-skills";
 import NotificationToast from "./components/notifaction-toast";
 import ModelComponent from "./components/model-component";
@@ -9,15 +11,70 @@ import DatePickerComponent from "./components/date-picker.component";
 
 function App() {
   return (
-    <div>
-      <TechSkills></TechSkills>
-      <NotificationToast></NotificationToast>
-      <ModelComponent></ModelComponent>
-      <ToolTipComponent></ToolTipComponent>
-      <CountUpNums></CountUpNums>
-      <CreditCardComponent></CreditCardComponent>
-      <DatePickerComponent></DatePickerComponent>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/tech-skills">Tech Skills</Link>
+          </li>
+          <li>
+            <Link to="/notification-toast">Notification Toast</Link>
+          </li>
+          <li>
+            <Link to="/model">Model</Link>
+          </li>
+          <li>
+            <Link to="/tool-tip">Tool Tip</Link>
+          </li>
+          <li>
+            <Link to="/count-up">Count Up</Link>
+          </li>
+          <li>
+            <Link to="/credit-card">Credit Card</Link>
+          </li>
+          <li>
+            <Link to="/date-picker">Date Picker</Link>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/tech-skills">
+            {" "}
+            <TechSkills />
+          </Route>
+          <Route path="/notification-toast">
+            {" "}
+            <NotificationToast />
+          </Route>
+          <Route path="/model">
+            {" "}
+            <ModelComponent />
+          </Route>
+          <Route path="/tool-tip">
+            {" "}
+            <ToolTipComponent />
+          </Route>
+          <Route path="/count-up">
+            {" "}
+            <CountUpNums />
+          </Route>
+          <Route path="/credit-card">
+            {" "}
+            <CreditCardComponent />
+          </Route>
+          <Route path="/date-picker">
+            {" "}
+            <DatePickerComponent />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/home.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to React Lib Practice</h2>
+    </div>
+  );
+}
+
+export default Home;
